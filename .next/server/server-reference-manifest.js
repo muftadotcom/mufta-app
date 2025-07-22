093c0ec643960311797d289628b7cb941800c372
Red Bee <havoc@Reds-MacBook-Pro.local>
2025-07-22 13:26:09 +0500

    Fix: Correct type error in Countdown timer for production build

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"53fkCaX+0RmZxTNc3kh6DJEf+yakuQ6xbfxgY4Y2Jew=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"zTYBd8BqV/KGqJ3JOGj2xRjdUFmUk/fZNJTXgajsNJ0=\"\n}"
